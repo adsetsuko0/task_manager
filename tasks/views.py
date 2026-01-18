638b72847f4f372fb6f3283ac0a937374771735e
adsetsuko0 <r41969827@gmail.com>
2026-01-18 17:22:37 +0300

    groups added need now to add ... and other

--- tasks/views.py
+++ tasks/views.py
@@ -12,7 +12,6 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-from .models import Projects_Group
 import json
 
 
